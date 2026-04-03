824b75a76d2e40e6f7a10ad164dc924c88a501fb
vishwanath-5 <vishwanathnani1@gmail.com>
2026-04-03 18:04:24 +0530

    Fixed api

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"status": "Backend running 🚀"})
 
 urlpatterns = [
+    path('', home),  # 👈 ADD THIS
+
     path('admin/', admin.site.urls),
 
     path('api/users/', include('users.urls')),
